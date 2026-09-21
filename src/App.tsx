82d967d199a2a985fb7e0b9cd97d0718ed332fe0
Goldragon <16520417@std.stei.itb.ac.id>
2026-09-21 13:02:19 +0700

    favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { supabase, getSupabaseImageUrl } from './lib/supabase'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import AdminLayout from './components/layout/AdminLayout'
@@ -15,6 +17,24 @@ import EventList from './pages/admin/EventList'
 import Settings from './pages/admin/Settings'
 
 export default function App() {
+  useEffect(() => {
+    const loadFavicon = async () => {
+      const { data } = await supabase
+        .from('site_settings')
+        .select('value')
+        .eq('key', 'logo_url')
+        .single()
+      if (data?.value) {
+        const url = getSupabaseImageUrl(data.value)
+        const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement || document.createElement('link')
+        link.rel = 'icon'
+        link.href = url
+        document.head.appendChild(link)
+      }
+    }
+    loadFavicon()
+  }, [])
+
   return (
     <div className="min-h-screen flex flex-col">
       <Routes>
